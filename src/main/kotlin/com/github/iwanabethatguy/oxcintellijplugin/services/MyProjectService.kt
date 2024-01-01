742d2a09e1a0f1afd269c475686e6a9688d30407
GitHub Action <action@github.com>
2024-01-01 11:30:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/iwanabethatguy/oxcintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/iwanabethatguy/oxcintellijplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.iwanabethatguy.oxcintellijplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.iwanabethatguy.oxcintellijplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/iwanabethatguy/oxcintellijplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/iwanabethatguy/oxcintellijplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.iwanabethatguy.oxcintellijplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.iwanabethatguy.oxcintellijplugin.MyBundle
+import com.github.iwanabethatguy.oxcintellijplugin.services.MyProjectService
 import javax.swing.JButton
 
 

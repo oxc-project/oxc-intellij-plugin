2408beaf737c311832966c3d44136a6fcd9b9858
Nick Rayburn <52075362+nrayburn-tech@users.noreply.github.com>
2024-10-09 02:43:51 -0500

    Update server descriptor to search the current project for the language server

--- src/main/kotlin/com/github/iwanabethatguy/oxcintellijplugin/lsp/OxcLspServerDescriptor.kt
+++ src/main/kotlin/com/github/iwanabethatguy/oxcintellijplugin/lsp/OxcLspServerDescriptor.kt
@@ -1,37 +1,59 @@
 package com.github.iwanabethatguy.oxcintellijplugin.lsp
 
-import com.intellij.execution.ExecutionException
 import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.lsp.api.ProjectWideLspServerDescriptor
-import com.intellij.openapi.fileTypes.FileType
-import com.intellij.platform.lsp.api.customization.LspDiagnosticsSupport
-import com.intellij.platform.lsp.api.requests.LspRequest
-import org.eclipse.lsp4j.ConfigurationItem
-import org.eclipse.lsp4j.InitializeParams
+import com.intellij.util.FileSearchUtil
+import java.time.Duration
 
+@Suppress("UnstableApiUsage")
 class OxcLspServerDescriptor(project: Project) : ProjectWideLspServerDescriptor(project, "Oxc") {
 
-    override fun isSupportedFile(file: VirtualFile):  Boolean {
-        thisLogger().warn("file.extension " + file.extension.toString())
-        return file.extension == "js" ||  file.extension == "jsx" || file.extension == "ts" || file.extension == "tsx"
+    companion object {
+
+        fun supportedFile(file: VirtualFile): Boolean {
+            return file.isInLocalFileSystem && listOf("js", "jsx", "ts", "tsx").contains(
+                file.extension)
+        }
+    }
+
+    override fun isSupportedFile(file: VirtualFile): Boolean {
+        thisLogger().warn(
+            "file.isInLocalFileSystem ${file.isInLocalFileSystem}, file.extension ${file.extension}")
+        return supportedFile(file)
     }
 
     override fun createCommandLine(): GeneralCommandLine {
         thisLogger().warn("Start oxc language server")
 
-        return GeneralCommandLine("oxc_language_server").apply {
-        }
+        return GeneralCommandLine(findBinary())
     }
 
     override fun createInitializationOptions(): Any? {
         val options = super.createInitializationOptions()
-        thisLogger().warn("get oxc configuration" + options.toString())
+        thisLogger().warn("get oxc configuration $options")
         return options
     }
 
+    private fun findBinary(): String {
+        val foundBinaries = roots.map {
+            val fileQuery = FileSearchUtil.findFileRecursively(it, "oxc_language_server", 10,
+                Duration.ofSeconds(5).toMillis())
+            return@map fileQuery.findFirst()
+        }.filterNotNull()
+        if (foundBinaries.size == 1) {
+            return foundBinaries.single().path
+        }
+        if (foundBinaries.size > 1) {
+            thisLogger().warn("Found multiple binaries")
+            return foundBinaries.single().path
+        }
+
+        thisLogger().warn("Unable to find binaries, falling back to PATH")
+        return "oxc_language_server"
+    }
 
     override val lspGoToDefinitionSupport = false
 
@@ -40,6 +62,5 @@ class OxcLspServerDescriptor(project: Project) : ProjectWideLspServerDescriptor(
     override val lspFormattingSupport = null
 
     override val lspHoverSupport = false
-
 
 }

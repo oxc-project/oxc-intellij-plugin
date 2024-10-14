5964246161cc9b5fddf35eb6d907d452d6098452
Nicholas Rayburn <52075362+nrayburn-tech@users.noreply.github.com>
2024-10-14 10:46:23 -0500

    feat: Add a custom JSON schema provider for Oxc config files (#66)

---
+++ src/main/kotlin/com/github/iwanabethatguy/oxcintellijplugin/Constants.kt
@@ -0,0 +1,7 @@
+package com.github.iwanabethatguy.oxcintellijplugin
+
+object Constants {
+
+    val CONFIG_FILES = listOf("oxlintrc.json", "oxlint.json", ".oxlintrc.json", ".oxlint.json",
+        ".oxlintrc")
+}

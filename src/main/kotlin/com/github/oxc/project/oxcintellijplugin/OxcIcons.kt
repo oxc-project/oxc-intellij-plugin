f1cca6bb2983dcad968b00db58554a3a4b774c3a
Nick Rayburn <52075362+nrayburn-tech@users.noreply.github.com>
2025-04-14 00:32:01 -0500

    fix: Rename icon to meet JetBrains design standards (camel case icon file names)

--- src/main/kotlin/com/github/oxc/project/oxcintellijplugin/OxcIcons.kt
+++ src/main/kotlin/com/github/oxc/project/oxcintellijplugin/OxcIcons.kt
@@ -5,6 +5,6 @@ import com.intellij.openapi.util.IconLoader
 object OxcIcons {
 
     @JvmField
-    val OxcRound = IconLoader.getIcon("/icons/oxlint_round.svg", javaClass)
+    val OxcRound = IconLoader.getIcon("/icons/oxcRound.svg", javaClass)
 
 }

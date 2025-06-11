7c155ac00adbf5b2844a8de6009dd5b82e7a6833
Nicholas Rayburn <52075362+nrayburn-tech@users.noreply.github.com>
2025-06-10 22:20:02 -0500

    fix: Use project root as the default directory if an oxlint config file is not available (#222)
    * fix: Use project root as the default directory if an oxlint config file is not available
    
    * test: Add retries to language server testing to better handle the async nature of the tests

---
+++ src/test/testData/highlighting/no-config/index.expected.js
@@ -0,0 +1,8 @@
+console.log('Happy developing ✨')
+
+<warning descr="oxc: `debugger` statement is not allowed
+help: Remove the debugger statement eslint(no-debugger)">debugger</warning>
+<warning descr="oxc: Do not use `new Array(singleArgument)`.
+help: It's not clear whether the argument is meant to be the length of the array or the only element. If the argument is the array's length, consider using `Array.from({ length: n })`. If the argument is the only element, use `[element]`. eslint-plugin-unicorn(no-new-array)">new Array([])</warning>
+
+if (foo) foo++;

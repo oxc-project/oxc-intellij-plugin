79e3ffba009b9c0329f7493109096f4099ff1801
Nicholas Rayburn <52075362+nrayburn-tech@users.noreply.github.com>
2025-07-03 23:02:05 -0500

    fix: Always use the location of package.json with oxlint installed as the project root (#240)

--- src/main/kotlin/com/github/oxc/project/oxcintellijplugin/lsp/OxcLspServerSupportProvider.kt
+++ src/main/kotlin/com/github/oxc/project/oxcintellijplugin/lsp/OxcLspServerSupportProvider.kt
@@ -2,18 +2,15 @@ package com.github.oxc.project.oxcintellijplugin.lsp
 
 import com.github.oxc.project.oxcintellijplugin.OxcIcons
 import com.github.oxc.project.oxcintellijplugin.OxcPackage
-import com.github.oxc.project.oxcintellijplugin.extensions.findNearestOxcConfig
-import com.github.oxc.project.oxcintellijplugin.extensions.findNearestPackageJson
 import com.github.oxc.project.oxcintellijplugin.settings.OxcConfigurable
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.project.guessProjectDir
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.platform.lsp.api.LspServer
 import com.intellij.platform.lsp.api.LspServerSupportProvider
 import com.intellij.platform.lsp.api.lsWidget.LspServerWidgetItem
-import java.io.File
+import kotlin.io.path.Path
 
 @Suppress("UnstableApiUsage")
 class OxcLspServerSupportProvider : LspServerSupportProvider {
@@ -26,17 +23,9 @@ class OxcLspServerSupportProvider : LspServerSupportProvider {
         if (!oxc.isEnabled()) {
             return
         }
-        val configPath = oxc.configPath()
         val executable = oxc.binaryPath(file) ?: return
-
-        val projectRootDir = project.guessProjectDir() ?: return
-        val root: VirtualFile
-        if (configPath?.isNotEmpty() == true) {
-            val configVirtualFile = VirtualFileManager.getInstance().findFileByNioPath(File(configPath).toPath()) ?: return
-            root = configVirtualFile.findNearestPackageJson(projectRootDir)?.parent ?: return
-        } else {
-            root = file.findNearestOxcConfig(projectRootDir)?.parent ?: projectRootDir
-        }
+        val nodePackage = oxc.getPackage(file) ?: return
+        val root = VirtualFileManager.getInstance().findFileByNioPath(Path(nodePackage.systemIndependentPath))?.parent?.parent ?: return
 
         serverStarter.ensureServerStarted(OxcLspServerDescriptor(project, root, executable))
     }

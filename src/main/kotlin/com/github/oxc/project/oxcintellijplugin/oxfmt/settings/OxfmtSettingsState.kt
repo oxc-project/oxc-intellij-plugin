035ea7dcf841425173dc870bbf4bc3ab2e81fc1b
Noel Kim (김민혁) <minhyuk@clobe.com>
2026-04-06 09:46:37 +0900

    chore(oxfmt): Add `.json`, `.jsonc` to default supported extensions
    Signed-off-by: Noel Kim (김민혁) <minhyuk@clobe.com>

--- src/main/kotlin/com/github/oxc/project/oxcintellijplugin/oxfmt/settings/OxfmtSettingsState.kt
+++ src/main/kotlin/com/github/oxc/project/oxcintellijplugin/oxfmt/settings/OxfmtSettingsState.kt
@@ -26,6 +26,7 @@ class OxfmtSettingsState : BaseState() {
         val DEFAULT_EXTENSION_LIST = listOf(
             ".js", ".jsx", ".cjs", ".mjs",
             ".ts", ".tsx", ".cts", ".mts",
+            ".json", ".jsonc"
         )
     }
 }
